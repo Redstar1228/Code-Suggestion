2a2ef44e46cb3fc3840a881c14f8294492025be6
Redstar <aliakbarzarei41@gmail.com>
2025-04-05 08:33:22 +0330

    Update main_host.py

--- Back-end/main_host.py
+++ Back-end/main_host.py
@@ -1,9 +1,10 @@
 from flask import *
+from main_code import main
 
 app = Flask(__name__)
 
 @app.route("/")
 def suggest():
-    pass
+    main()
 if __name__ == "__main__":
     app.run(host = "0.0.0.0")
